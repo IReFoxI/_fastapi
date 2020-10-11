5fff41bdcb8cd82de836602ff54487a1d99493f0
kakbari <kamil_akbari@govitall.com>
2020-10-12 01:03:11 +0300

    --help

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import uvicorn
 from fastapi import FastAPI
 # from pydantic import BaseModel
-import time
+# import time
+import asyncio
 
 from fastapi import FastAPI
 
@@ -10,7 +11,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    time.sleep(10)
+    # time.sleep(10)
     return {"message": "Hello World"}
 
 
@@ -21,8 +22,25 @@ async def read_user_me():
 
 @app.get("/users/{user_id}")
 async def read_user(user_id: str):
-    return {"user_id": user_id}
+    list = []
+    list2 = []
 
+    async def first():
+        for i in range(2):
+
+            list.append(i)
+        await asyncio.sleep(3)
+        print(list)
+
+    async def second():
+        for i in range(10):
+            list2.append(i)
+        print(list2)
+
+    await first()
+    await second()
+    
+    return {"user_id": user_id, 'list': list, 'list2':list2}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8080)
