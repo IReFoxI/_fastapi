b08622579422c94c349c2c92774d1bd672c646d3
IReFoxI <ra.mailmaster@gmail.com>
2020-10-18 03:11:49 +0300

    add cities datetime info

--- main.py
+++ main.py
@@ -1,26 +1,54 @@
 import uvicorn
 from fastapi import FastAPI
-# from pydantic import BaseModel
+from pydantic import BaseModel
 # import time
-import asyncio
+from requests_html import HTMLSession
 
 
 app = FastAPI()
+session = HTMLSession()
+
+
+db = []
+
+
+class City(BaseModel):
+    name: str
+    timezone: str
 
 
 @app.get("/")
-def home():
-    # time.sleep(10)
+def index():
     return {"message": "Hello World"}
 
+@app.get('/cities')
+def get_cities():
+    results = []
+    for city in db:
+        with session.get(f"http://worldtimeapi.org/api/timezone/{city['timezone']}") as response:
+            results.append({'name': city['name'],
+                            'timezone': city['timezone'],
+                            'current_time': response.json()['datetime']
+                            })
+    return results
 
-@app.get("/{pk}")
-async def get_item(pk: int, q: str = None):
-    return {"key": pk, "q": q}
+@app.get('/cities/{city_id}')
+def get_citi(city_id: int):
+    city = db[city_id]
+    with session.get(f"http://worldtimeapi.org/api/timezone/{city['timezone']}") as response:
+        return {'name': city['name'],
+                'timezone': city['timezone'],
+                'current_time': response.json()['datetime']}
 
-@app.get('/user/{pk}/items/{item}/')
-async def get_user_item(pk: int, item: str):
-    return {"user": pk, "item": item}
+@app.post('/cities')
+def create_city(city: City):
+    db.append(city.dict())
+    return db
+
+@app.delete('/cities/{city_id}')
+def delete_city(city_id: int):
+    db.pop(city_id)
+    return 'Deleted'
 
 
 if __name__ == "__main__":
